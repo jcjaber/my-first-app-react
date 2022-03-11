c22ebb9cce1873aa8a38f712cc70d596e5a2b2a1
Cassio Jaber <jcjaber@gmail.com>
2022-03-10 21:04:46 -0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <Header />
+        <p>Hello React!</p>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+  return (
+    <footer>
+      <p>All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer;
