7d9f2433311a7adce40a4a85c456a3c580cbe3e1
Cassio Jaber <jcjaber@gmail.com>
2022-03-22 14:15:09 -0300

    Added theme support

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { GlobalStyle } from "./styles";
+import { ThemeProvider } from 'styled-components';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { GlobalStyle } from './styles';
+import { defaultTheme } from './themes';
 
-const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <div>
-        <header>
-          <Header />
-          <p>Hello React!</p>
-        </header>
-        <Footer />
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+    <div>
+      <header>
+        <Header />
+        <p>Hello React!</p>
+      </header>
+      <Footer />
+    </div>
+  </ThemeProvider>
+);
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
+  document.getElementById('root'),
 );
